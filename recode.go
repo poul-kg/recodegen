ad83af197b58b50efce921cce7142305b800c400
Pavel Kostenko <poul.kg@gmail.com>
2023-05-19 09:46:03 +0600

    chore: improved generation of object field types

--- recode.go
+++ recode.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
@@ -32,8 +34,9 @@ func main() {
 	}
 
 	// Traverse and process the AST (example: print type names)
-	var enumOnly string
-	var typesOnly string
+	var enumOnly = ""
+	var typesOnly = ""
+	var objectsOnly = ""
 	for _, def := range doc.Types {
 		if def.Kind == ast.Enum {
 			enumOnly += genEnum(def)
@@ -41,16 +44,112 @@ func main() {
 		if def.Kind == ast.InputObject {
 			typesOnly += genInputObject(def)
 		}
+		if def.Kind == ast.Object {
+			objectsOnly += generateObject(def)
+		}
 	}
-	writeFile("types.ts", getTypesHeader()+enumOnly+typesOnly)
+	writeFile("types.ts", getTypesHeader()+enumOnly+typesOnly+objectsOnly)
+}
+
+type TemplateField struct {
+	Name        string
+	Type        string
+	Description string
+}
+
+func getTemplateField(field *ast.FieldDefinition) TemplateField {
+	return TemplateField{
+		Name:        field.Name,
+		Type:        field.Type.String(),
+		Description: field.Description,
+	}
+}
+
+func getTemplateFields(def *ast.Definition) []TemplateField {
+	var fields []TemplateField
+	for _, field := range def.Fields {
+		fields = append(fields, getTemplateField(field))
+	}
+	return fields
+}
+
+// tried to use templates, don't like white space it introduces
+// and weird {{- }} and {{ -}} syntax I need to use
+func genObjectViaTemplate(def *ast.Definition) string {
+	type TemplateInput struct {
+		Name        string
+		Description string
+		Fields      []TemplateField
+	}
+	srcTemplate := `
+{{ if .Description }}/** {{.Description }} */{{end}}
+export type {{.Name}} = {
+{{- range .Fields }}
+  {{ if .Description -}}
+  /** {{.Description }} */
+  {{- end }}
+  {{.Name}}: {{.Type}};
+{{- end}}
+}
+`
+	templateInput := TemplateInput{}
+	templateInput.Name = normalizedName(def.Name)
+	templateInput.Description = def.Description
+	templateInput.Fields = getTemplateFields(def)
+
+	tmpl, err := template.New("test").Parse(srcTemplate)
+	if err != nil {
+		panic(err)
+	}
+	var outputBuffer bytes.Buffer
+	if err := tmpl.Execute(&outputBuffer, templateInput); err != nil {
+		panic(err)
+	}
+	return outputBuffer.String()
+}
+
+func generateObject(def *ast.Definition) string {
+	objectName := normalizedName(def.Name)
+	desc := generateDesc(def.Description)
+	header := fmt.Sprintf("\nexport type %s = {", objectName)
+	if len(desc) > 0 {
+		header = "\n" + desc + header
+	}
+	body := ""
+	footer := "\n};\n"
+
+	for _, field := range def.Fields {
+		if field.Name == "__schema" {
+			continue
+		}
+		if field.Name == "__type" {
+			continue
+		}
+		if len(field.Description) > 0 {
+			body += "\n" + spacing + generateDesc(field.Description)
+		}
+		body += "\n" + spacing + generateFieldName(field) + ": " + generateFieldType(field.Type) + ";"
+	}
+	return header + body + footer
+}
+
+func generateDesc(desc string) string {
+	if len(desc) > 0 {
+		return fmt.Sprintf("/** %s */", desc)
+	}
+	return ""
 }
 
 func genEnum(def *ast.Definition) string {
-	var template = `export enum %s {
+	var tmpl = `export enum %s {
 %s
 }
 
 `
+	desc := generateDesc(def.Description)
+	if len(desc) > 0 {
+		tmpl = desc + "\n" + tmpl
+	}
 	var enumValues []string
 	for _, enumVal := range def.EnumValues {
 		desc := ""
@@ -62,11 +161,11 @@ func genEnum(def *ast.Definition) string {
 		enumItem := fmt.Sprintf("%s\n%s%s = '%s',", desc, spacing, getEnumItemName(enumVal.Name), enumVal.Name)
 		enumValues = append(enumValues, enumItem)
 	}
-	enumName := getTypeName(def.Name)
-	return fmt.Sprintf(template, enumName, strings.Join(enumValues, "\n"))
+	enumName := normalizedName(def.Name)
+	return fmt.Sprintf(tmpl, enumName, strings.Join(enumValues, "\n"))
 }
 
-func getTypeName(snakeCase string) string {
+func normalizedName(snakeCase string) string {
 	words := strings.Split(snakeCase, "_")
 	for i, word := range words {
 		words[i] = strings.Title(word)
@@ -116,7 +215,12 @@ func writeFile(fileName string, data string) {
 		log.Fatal(err)
 	}
 
-	defer f.Close()
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(f)
 
 	_, err2 := f.WriteString(data)
 
@@ -127,66 +231,72 @@ func writeFile(fileName string, data string) {
 
 func genInputObject(def *ast.Definition) string {
 	desc := genDesc(def)
-	start := desc + "export type " + getTypeName(def.Name) + " = {\n"
+	start := desc + "export type " + normalizedName(def.Name) + " = {\n"
 	var fields []string
 	for _, field := range def.Fields {
-		fieldStr := "  " + getFieldName(field) + ": " + genInputType(field.Type) + ";\n"
+		fieldStr := "  " + generateFieldName(field) + ": " + genInputFieldType(field.Type) + ";\n"
 		fields = append(fields, fieldStr)
 	}
 	return start + strings.Join(fields, "") + "};\n\n"
 }
 
-func genInputType(astType *ast.Type) string {
-	if astType.String() == "[Boolean!]" {
-		return "InputMaybe<Array<Scalars['Boolean']>>"
-	}
-	if astType.String() == "Boolean" {
-		return "InputMaybe<Scalars['Boolean']>"
-	}
-	if astType.String() == "[String!]" {
-		return "InputMaybe<Array<Scalars['String']>>"
-	}
-	if astType.String() == "String" {
-		return "InputMaybe<Scalars['String']>"
-	}
-	if astType.String() == "Int!" {
-		return "Scalars['Int']"
-	}
-	if astType.String() == "Int" {
-		return "InputMaybe<Scalars['Int']>"
-	}
-	if astType.String() == "[Int!]" {
-		return "InputMaybe<Array<Scalars['Int']>>"
-	}
-	if astType.String() == "float8" {
-		return "Maybe<Scalars['float8']>"
-	}
-	if astType.String() == "[float8!]" {
-		return "InputMaybe<Array<Scalars['float8']>>"
-	}
-	if astType.String() == "bigint" {
-		return "Maybe<Scalars['bigint']>"
-	}
-	if astType.String() == "[bigint!]" {
-		return "InputMaybe<Array<Scalars['bigint']>>"
-	}
-	if astType.String() == "timestamp" {
-		return "Maybe<Scalars['timestamp']>"
-	}
-	if astType.String() == "[timestamp!]" {
-		return "InputMaybe<Array<Scalars['timestamp']>>"
-	}
-	if astType.String() == "timestamptz" {
-		return "Maybe<Scalars['timestamptz']>"
-	}
-	if astType.String() == "[timestamptz!]" {
-		return "InputMaybe<Array<Scalars['timestamptz']>>"
+func generateFieldType(astType *ast.Type) string {
+	normalName := wrapScalar(normalizedName(astType.Name()))
+
+	if astType.NamedType != "" {
+		if astType.NonNull == false {
+			normalName = "Maybe<" + normalName + ">"
+		}
+		return normalName
 	}
 
-	return getTypeName(astType.Name())
+	if astType.NonNull == false {
+		normalName = "Maybe<Array<" + normalName + ">>"
+	}
+
+	return "Array<" + normalName + ">"
 }
 
-func getFieldName(astFieldDef *ast.FieldDefinition) string {
+// given type like "Int" will wrap it into "Scalars['Int']"
+// if given type is not scalar, return as is
+func wrapScalar(typeName string) string {
+	scalars := []string{"Boolean", "String", "Int", "Float8", "Float", "Bigint", "Timestamp", "Timestamptz"}
+	for _, scalar := range scalars {
+		if typeName == scalar {
+			switch scalar {
+			case "Bigint":
+				fallthrough
+			case "Float8":
+				fallthrough
+			case "Timestamp":
+				fallthrough
+			case "Timestamptz":
+				return "Scalars['" + strings.ToLower(typeName) + "']"
+			}
+			return "Scalars['" + typeName + "']"
+		}
+	}
+	return typeName
+}
+
+func genInputFieldType(astType *ast.Type) string {
+	normalName := wrapScalar(normalizedName(astType.Name()))
+
+	if astType.NamedType != "" {
+		if astType.NonNull == false {
+			normalName = "InputMaybe<" + normalName + ">"
+		}
+		return normalName
+	}
+
+	if astType.NonNull == false {
+		normalName = "InputMaybe<Array<" + normalName + ">>"
+	}
+
+	return "Array<" + normalName + ">"
+}
+
+func generateFieldName(astFieldDef *ast.FieldDefinition) string {
 	return astFieldDef.Name + genNullable(astFieldDef)
 }
 
